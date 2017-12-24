bb4cfc5b0ef74064fbc0e7c5b961b5b36fe11eaf
Vincent Yu <vinjyu1@gmail.com>
2017-12-24 17:34:58 -0500

    deploy work

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from bookapp import app
+import os
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(host="0.0.0.0", debug=True, port=int(os.environ.get("PORT", 5000)))
